c4aaac70fb9d22d18d8e46f6d3abda35520ffc33
Alabi Busayo <busayo_jee@yahoo.com>
2021-02-05 20:48:23 +0200

    Router Info for Cisco CSR1000V router

---
+++ netconf/router_info.py
@@ -0,0 +1,7 @@
+#router's credentials of the the cisco csr1000v
+router = {"host":"ios-xe-mgmt.cisco.com","port":"10000",
+"username":"developer","password":"C1sco12345"}
+
+#This router2 has a problem with the yang datamodel specified
+router2 = {"host":"sandbox-iosxe-latest-1.cisco.com","port":"830","username":"developer","password":"C1sco12345"}
+
